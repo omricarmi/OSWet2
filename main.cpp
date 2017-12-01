57dbd6237c98a87199148a8ffb751c2755d22c13
Omri Carmi <omricarmi@gmail.com>
2017-12-01 08:06:17 +0200

    new project

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+//TODO declare list of bank accounts
 
 int main(int argc, char* argv[]){
 
     //validate programm min param
     if(argc<2){
+        cout << "too few params\n";
         return 1;
     }
 
@@ -19,9 +24,9 @@ int main(int argc, char* argv[]){
     for(int i=0;i<N;++i){
         printf("%s\n",inputFiles[i]);
     }
-    
+
     //TODO on 28/11/17 by Omri Carmi : init ATMs pthreads
-    
-    
+
+
     return 0;
 }
--- types.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by compm on 28/11/17.
-//
-
-#ifndef OSWET2_TYPES_H
-#define OSWET2_TYPES_H
-
-#endif //OSWET2_TYPES_H
