46ecb65da8c0df0827eb26613379852c9b0057de
Omri Carmi <omricarmi@gmail.com>
2017-12-13 16:25:24 +0200

    hot fix of common.h

---
+++ common.cpp
@@ -0,0 +1,16 @@
+//
+// Created by compm on 13/12/17.
+//
+
+#include "common.h"
+
+void logd(string msg){
+    if(IS_DEBUG) {
+        std::cout << "debug: " << msg << endl;
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+}
