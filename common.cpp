10a9362e85d0962e1d37ce13e200ca44ca9fcd92
Omri Carmi <omricarmi@gmail.com>
2017-12-13 23:02:07 +0200

    working :-) :-) :-)

--- common.cpp
+++ common.cpp
@@ -13,4 +13,5 @@ void logd(string msg){
 std::vector<std::string> split(const std::string &s, char delim) {
     std::vector<std::string> elems;
     split(s, delim, std::back_inserter(elems));
+    return elems;
 }
